184443a2587ce5fe28d1619464e31a1179c74ccb
Gungun Sharma <sharmagungun970@gmail.com>
2024-10-15 13:58:43 +0530

    f

--- day4.py
+++ day4.py
@@ -1,1 +1,5 @@
-print("gungun")
+import pandas
+
+# Read and work with a file named 'words.csv'
+df = pandas.read_csv('words.csv')
+print(df) # This will display first few rows from the words.csv file

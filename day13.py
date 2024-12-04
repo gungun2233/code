4fe946e1596afbebbad4478c97ca12a4fb3f2b95
Gungun Sharma <sharmagungun970@gmail.com>
2024-12-05 07:22:36 +1100

    f

--- day13.py
+++ day13.py
@@ -2,7 +2,6 @@ fruit = "Mango"
 mangoLen = len(fruit)
 print(fruit[0:4]) # including 0 but not 4
 print(fruit[1:4]) # including 1 but not 4
-print(fruit[0:-3])
 print(fruit[:len(fruit)-3])
 print(fruit[-1:len(fruit) - 3])
 print(fruit[-3:-1])

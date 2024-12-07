a9ac1b15676bb4f1157a24aa47d3f42c815e7958
Gungun Sharma <sharmagungun970@gmail.com>
2024-12-07 07:34:57 +0000

    f

--- day7.py
+++ day7.py
@@ -8,7 +8,6 @@ a1 = 9
 print(a + a1)
 print("The type of a is ", type(a))
 print("The type of b is ", type(b))
-print("The type of c is ", type(c))
 
 
 list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]

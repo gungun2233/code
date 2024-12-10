4893c64060d23dc90c8192fe5c2f47f61307e21e
Gungun Sharma <sharmagungun970@gmail.com>
2024-12-10 06:33:58 +0000

    f

--- day9.py
+++ day9.py
@@ -4,4 +4,4 @@ b = 3
 print("The value of", a, "+", 3, "is: ", a + b)
 print("The value of", a, "-", 3, "is: ", a - b)
 print("The value of", a, "*", 3, "is: ", a * b)
-print("The value of", a, "/", 3, "is: ", a / b)
+

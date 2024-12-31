cfc58dc2fb05db512b1a7faf27850dc0746643ce
Gungun Sharma <sharmagungun970@gmail.com>
2024-12-31 08:24:12 +0000

    f

--- day6.py
+++ day6.py
@@ -1,8 +1,6 @@
 #This is a 'Single-Line Comment'
 print("This is a print statement.")
-"""This is an if-else statement.
-It will execute a block of code if a specified condition is true.
-If the condition is false then it will execute another block of code."""
+
 p = 7
 if (p > 5):
     print("p is greater than 5.")

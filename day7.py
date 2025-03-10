fc0a378d8f3d2edca9446ddb9f552030866cf9a3
Gungun Sharma <sharmagungun970@gmail.com>
2025-03-10 08:00:53 +0000

    f

--- day7.py
+++ day7.py
@@ -9,7 +9,6 @@ print("The type of a is ", type(a))
 print("The type of b is ", type(b))
 list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
 print(list1)
-
 tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
 print(tuple1)
 

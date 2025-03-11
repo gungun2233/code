598a3e96bcc14c9e5f072826da9b3e8eb556f195
Gungun Sharma <sharmagungun970@gmail.com>
2025-03-11 00:21:12 +0000

    f

--- day7.py
+++ day7.py
@@ -11,6 +11,5 @@ list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
 print(list1)
 tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
 print(tuple1)
-
 dict1 = {"name":"Sakshi", "age":20, "canVote":True}
 print(dict1)
